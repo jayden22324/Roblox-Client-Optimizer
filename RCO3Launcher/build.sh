34042fe584eff59c6e67b46774fc0b7291cf28f7
Exponential-Workload <cc@aaathats3as.com>
2023-05-24 23:55:29 +0200

    feat: build macos on macos

--- RCO3Launcher/build.sh
+++ RCO3Launcher/build.sh
@@ -10,7 +10,7 @@ curl https://sh.rustup.rs | sh -s -- --no-modify-path -y
 source $HOME/.cargo/env
 
 # Install target platforms
-rustup target add x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu x86_64-apple-darwin aarch64-apple-darwin x86_64-pc-windows-msvc
+rustup target add x86_64-unknown-linux-gnu x86_64-pc-windows-msvc # x86_64-apple-darwin aarch64-apple-darwin aarch64-unknown-linux-gnu
 
 # Build for each platform
 cargo build --release --target=x86_64-unknown-linux-gnu
@@ -23,16 +23,16 @@ fi
 #     echo "Build failed for aarch64-unknown-linux-gnu"
 #     exit 1
 # fi
-cargo build --release --target=x86_64-apple-darwin
-if [ $? -ne 0 ]; then
-    echo "Build failed for x86_64-apple-darwin"
-    exit 1
-fi
-cargo build --release --target=aarch64-apple-darwin
-if [ $? -ne 0 ]; then
-    echo "Build failed for aarch64-apple-darwin"
-    exit 1
-fi
+# cargo build --release --target=x86_64-apple-darwin
+# if [ $? -ne 0 ]; then
+#     echo "Build failed for x86_64-apple-darwin"
+#     exit 1
+# fi
+# cargo build --release --target=aarch64-apple-darwin
+# if [ $? -ne 0 ]; then
+#     echo "Build failed for aarch64-apple-darwin"
+#     exit 1
+# fi
 cargo build --release --target=x86_64-pc-windows-msvc
 if [ $? -ne 0 ]; then
     echo "Build failed for x86_64-pc-windows-msvc"
