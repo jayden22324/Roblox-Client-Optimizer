202efa87c22dacdbc7aaed9f8488f821898a3257
Exponential-Workload <cc@aaathats3as.com>
2023-05-24 23:47:47 +0200

    fix: i'll deal with aarch64linux later

--- RCO3Launcher/build.sh
+++ RCO3Launcher/build.sh
@@ -18,11 +18,11 @@ if [ $? -ne 0 ]; then
     echo "Build failed for x86_64-unknown-linux-gnu"
     exit 1
 fi
-cargo build --release --target=aarch64-unknown-linux-gnu
-if [ $? -ne 0 ]; then
-    echo "Build failed for aarch64-unknown-linux-gnu"
-    exit 1
-fi
+# cargo build --release --target=aarch64-unknown-linux-gnu
+# if [ $? -ne 0 ]; then
+#     echo "Build failed for aarch64-unknown-linux-gnu"
+#     exit 1
+# fi
 cargo build --release --target=x86_64-apple-darwin
 if [ $? -ne 0 ]; then
     echo "Build failed for x86_64-apple-darwin"
