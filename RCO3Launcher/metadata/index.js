3695d4df731824b09dd017967409a10922914b4e
Exponential-Workload <cc@aaathats3as.com>
2023-05-25 07:32:18 +0200

    fix: metadata

--- RCO3Launcher/metadata/index.js
+++ RCO3Launcher/metadata/index.js
@@ -1,10 +1,12 @@
 if (!process.argv[2]) throw new Error("No input file specified!")
 
 const fs = require('fs');
+const path = require('path');
 const memes = JSON.parse(fs.readFileSync(__dirname + '/memes.json', 'utf8'));
 const getMeme = () => memes[Math.floor(Math.random() * memes.length)];
 
 import('resedit-cli').then(resedit => {
+  const ico = path.resolve(process.argv[3] ?? "icon.ico");
   resedit.default({
     in: process.argv[2],
     out: process.argv[2],
@@ -12,7 +14,18 @@ import('resedit-cli').then(resedit => {
     "file-description": Math.random() > 0.75 ? getMeme() : "Launches RCO3 using NodeJS. That's it.",
     "allowShrink": true,
     "allowGrow": true,
-    "icon": process.argv[3] ?? "icon.ico",
+    "icons": [
+      {
+        sourceFile: ico,
+      },
+      {
+        sourceFile: ico,
+        id: 1
+      }
+    ],
+    "icon": [
+      ico,
+    ],
     "file-version": "69.69.69.69",
     "product-version": "420.420.420.420",
     "original-filename": "rco-moment.exe",
