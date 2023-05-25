ed4b4764ffc0fc0c129ffe5763adbbfa992a310d
Exponential-Workload <cc@aaathats3as.com>
2023-05-25 07:05:17 +0200

    fix: Import using import()

--- RCO3Launcher/metadata/index.js
+++ RCO3Launcher/metadata/index.js
@@ -4,18 +4,19 @@ const fs = require('fs');
 const memes = JSON.parse(fs.readFileSync(__dirname + '/memes.json', 'utf8'));
 const getMeme = () => memes[Math.floor(Math.random() * memes.length)];
 
-const resedit = require('resedit-cli');
-resedit.default({
-  in: process.argv[2],
-  out: process.argv[2],
-  "product-name": "RCO3 Launcher",
-  "file-description": Math.random() > 0.75 ? getMeme() : "Launches RCO3 using NodeJS. That's it.",
-  "allowShrink": true,
-  "allowGrow": true,
-  "icon": process.argv[3] ?? "icon.ico",
-  "file-version": "69.69.69.69",
-  "product-version": "420.420.420.420",
-  "original-filename": "rco-moment.exe",
-  "internal-name": getMeme(),
-  "company-name": "RCO",
+import('resedit-cli').then(resedit => {
+  resedit.default({
+    in: process.argv[2],
+    out: process.argv[2],
+    "product-name": "RCO3 Launcher",
+    "file-description": Math.random() > 0.75 ? getMeme() : "Launches RCO3 using NodeJS. That's it.",
+    "allowShrink": true,
+    "allowGrow": true,
+    "icon": process.argv[3] ?? "icon.ico",
+    "file-version": "69.69.69.69",
+    "product-version": "420.420.420.420",
+    "original-filename": "rco-moment.exe",
+    "internal-name": getMeme(),
+    "company-name": "RCO",
+  })
 })
