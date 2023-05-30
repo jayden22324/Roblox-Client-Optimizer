e4dad0bdc1afc574abe3218ba6d8fda64bd4966e
Exponential-Workload <cc@aaathats3as.com>
2023-05-30 09:47:20 +0200

    chore: https://cdn.discordapp.com/attachments/1068827149848559637/1113010658347331604/image.png

--- RBX2Appdata/apply-manifest.bat
+++ RBX2Appdata/apply-manifest.bat
@@ -1,1 +1,3 @@
-resedit rbx2appdata.exe rbx2appdata.exe --raw 24,1,@res/app.manifest --allow-shrink
+copy rbx2appdata.exe rbx2appdata2.exe
+resedit rbx2appdata2.exe rbx2appdata.exe --raw 24,1,@res/app.manifest --allow-shrink
+del rbx2appdata2.exe
